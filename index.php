898ad94dfee4e4e3b7db7577b573490fbaad29d5
Janish Suneja <js2629@njit.edu>
2024-04-15 18:16:24 -0400

    Added styles.css

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
+<!-- This will be the FRONTEND for the project. -->
 <!DOCTYPE html>
 <html>
     <head>
-        <title>This is my internet channel</title>
+        <title>CS331 Database Project</title>
+        <meta charset="UTF-8"/>
+        <link rel="stylesheet" href="styles.css">
     </head>
     <body>
         <h1></h1>
