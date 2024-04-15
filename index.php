ee780f72492e4d644f55061f280299d96c5995c1
Janish Suneja <js2629@njit.edu>
2024-04-15 18:21:20 -0400

    Added image, other meta tags

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <!-- This will be the FRONTEND for the project. -->
 <!DOCTYPE html>
-<html>
+<html lang="en-US">
     <head>
         <title>CS331 Database Project</title>
         <meta charset="UTF-8"/>
-        <link rel="stylesheet" href="styles.css">
+        <meta name="author" content="Janish Suneja and Ewan Lee"/>
+        <meta name="description" content="This is a web page that is a test for database connections through the server."/>
+        <link rel="stylesheet" href="styles.css"/>
+        <link rel="icon" href="favicon.ico" type="image/db_icon.png"/>
     </head>
     <body>
         <h1></h1>
