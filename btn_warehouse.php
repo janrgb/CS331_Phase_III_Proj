70e486d665c3462b0e06645e12cf4069d540ecf0
Janish Suneja <js2629@njit.edu>
2024-04-17 00:08:17 -0400

    Added responsive tables to each page.

--- btn_warehouse.php
+++ btn_warehouse.php
@@ -1,3 +1,14 @@
+<?php
+    /* Querying for table. */
+    include('connection_two.php');
+    $query = "SELECT * FROM WAREHOUSE_LOC";
+    $stmt = $dbh->prepare($query);
+    $stmt->execute();
+
+    /* Fetch as associative... */
+    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -8,5 +19,27 @@
     </head>
     <body>
         <h1>WAREHOUSES</h1>
+        <table>
+            <tr>
+                <?php
+                    $firstRow = reset($rows);
+                    foreach ($firstRow as $attribute => $value)
+                    {
+                        echo "<th>$attribute</th>";
+                    }
+                ?>
+            </tr>
+            <?php
+            foreach ($rows as $row)
+            {
+                echo "<tr>";
+                foreach ($row as $value)
+                {
+                    echo "<td>$value</td>";
+                }
+                echo "</tr>";
+            }
+            ?>
+        </table>
     </body>
 </html>
--- display.php
+++ display.php
@@ -2,14 +2,17 @@
     /* Depending on input type, display a different page. */
     if (isset($_POST['submit_w']))
     {
-        include_once('btn_warehouse.php');
+        header("Location:btn_warehouse.php");
+        // include_once('btn_warehouse.php');
     }
     else if(isset($_POST['submit_m']))
     {
-        include_once('btn_material.php');
+        header("Location:btn_material.php");
+        // include_once('btn_material.php');
     }
     else if(isset($_POST['submit_e']))
     {
-        include_once('btn_equip.php');
+        header("Location:btn_equip.php");
+        // include_once('btn_equip.php');
     }
 ?>
