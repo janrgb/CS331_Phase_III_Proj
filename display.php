70edf89987c5926bb84ffa5d6aa69a468d6be843
Janish Suneja <js2629@njit.edu>
2024-04-16 23:42:09 -0400

    fixed display.php

--- display.php
+++ display.php
@@ -1,16 +1,15 @@
 <?php
     /* Depending on input type, display a different page. */
-    if (isset($_POST['btn_warehouse']))
+    if (isset($_POST['submit_w']))
     {
         include_once('btn_warehouse.php');
     }
-    else if(isset($_POST['btn_material']))
+    else if(isset($_POST['submit_m']))
     {
         include_once('btn_material.php');
     }
-    else if(isset($_POST['btn_equip']))
+    else if(isset($_POST['submit_e']))
     {
         include_once('btn_equip.php');
     }
-
 ?>
